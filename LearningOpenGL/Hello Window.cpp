e3b5fca1f00f85a989582435e5a172c566ab92fe
nanmu <zhanyongjie4@gmail.com>
2025-05-21 22:14:05 +0800

    black window

--- LearningOpenGL/Hello Window.cpp
+++ LearningOpenGL/Hello Window.cpp
@@ -1,18 +1,40 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);//
 int main()
 {
+    //首先是要创建一个窗口，这里我们就是使用glfw来创建窗口，函数都是以glfw开头
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//设置OpenGL版本
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//设置OpenGL配置文件
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);//创建窗口
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);//设置窗口的上下文为当前线程主上下文
+
+
+	//调用OpenGL函数之前需要初始化GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+	glViewport(0, 0, 800, 600);//设置视口大小
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
+}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
